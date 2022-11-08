431ed9e88d489a5593dd5e7b51e879d5a023170c
Mohamad O Shtaya <mohamad.shtaya2000@gmail.com>
2022-11-08 15:22:08 +0200

    commit

--- src/main/java/com/quizplus/tasktwo/Service/CourseService.java
+++ src/main/java/com/quizplus/tasktwo/Service/CourseService.java
@@ -11,7 +11,6 @@ public class CourseService {
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     CourseRepo courseRepo;
-
     public CourseService(CourseRepo courseRepo) {
         this.courseRepo = courseRepo;
     }
@@ -19,17 +18,13 @@ public class CourseService {
         return courseRepo.finaAll();
     }
     public Course findById(int theId) {
-
         return courseRepo.findById(theId);
-
     }
     public void save(Course course){
         course.setCourseId(0);
         courseRepo.save(course);
     }
     public Course Update(Course course){
-       //update course set courseCapacity=?, courseName=?, date_=?, isAvilable=?, teacherName=? where courseId=?
-        //String query = "Update masterusertable set username='" + txtName.Text + "',and set password='" + txtpassword.Text + "',and set phoneno='" + txtphoneno.Text + "',and set adress='" + rtxtAdress.Text + "' where userid ='" + txtuserid.Text + "'";
         courseRepo.save(course);
         return course;
     }
